0952502e78fbfa07d7e74cc7b9ef1b3d4a49934a
themkrv <themkrv@gmail.com>
2025-06-06 13:27:11 +0000

    Deploy ready: nginx, gunicorn, env, infra

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -2,13 +2,14 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = '/static_backend/'
+STATIC_ROOT = BASE_DIR / 'static_backend'
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['goidasmert.ddns.net', '127.0.0.1', 'localhost']
 
 # Application definition
 
